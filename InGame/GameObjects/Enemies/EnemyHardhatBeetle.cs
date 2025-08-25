cc0b251c9e8a938260217f11fb77e6a3483499d9
Scott Zimmerman <bighead.0@gmail.com>
2025-08-25 19:49:41 -0400

    Restore Hardhat Beetle knockback effect from piece of power and red tunic.

--- InGame/GameObjects/Enemies/EnemyHardhatBeetle.cs
+++ InGame/GameObjects/Enemies/EnemyHardhatBeetle.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using Microsoft.Xna.Framework;
 using ProjectZ.InGame.GameObjects.Base;
 using ProjectZ.InGame.GameObjects.Base.Components;
@@ -138,7 +139,6 @@ namespace ProjectZ.InGame.GameObjects.Enemies
         {
             if (_damageState.IsInDamageState())
                 return Values.HitCollision.None;
-            _damageState.SetDamageState(false);
 
             if (damageType == HitType.Boomerang || damageType == HitType.Hookshot)
             {
@@ -147,12 +147,14 @@ namespace ProjectZ.InGame.GameObjects.Enemies
                 _stunnedState.StartStun();
                 _damageField.IsActive = false;
             }
+            // Allows knockback effect from piece of power or red tunic.
+            if (pieceOfPower)
+                return _damageState.OnHit(gameObject, direction, damageType, 0, pieceOfPower);
 
+            _damageState.SetDamageState(false);
             _body.Velocity.X = direction.X * 3.0f;
             _body.Velocity.Y = direction.Y * 3.0f;
-
             Game1.GameManager.PlaySoundEffect("D360-09-09");
-
             return Values.HitCollision.Enemy;
         }
 
