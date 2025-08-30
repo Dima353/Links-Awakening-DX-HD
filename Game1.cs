6cfd9d8826d62310f6cba157cbcf6e080b766565
Scott Zimmerman <bighead.0@gmail.com>
2025-08-30 01:12:43 -0400

    UI Scale slider is fixed 1-11, Auto-Detect=11, scale only applies if valid for screen size.

--- Game1.cs
+++ Game1.cs
@@ -69,11 +69,6 @@ namespace ProjectZ
         public static int UiScale;
         public static int UiRtScale;
 
-        // Tracks changes in window scaling.
-        private int _lastPixels;
-        private int _lastScale;
-        private bool _wasUIAutoDetect;
-
         public static int RenderWidth;
         public static int RenderHeight;
 
@@ -916,45 +911,6 @@ namespace ProjectZ
             UpdateScale();
         }
 
-        private int CalculateUiScale(int CurUiScale, int CurScrScale)
-        {
-            // We hope this is never what is returned.
-            int returnScale = 1;
-            int totalPixels = WindowWidth * WindowHeight;
-
-            // Did the user have the UI scale set to auto-detect?
-            _wasUIAutoDetect = (CurUiScale == CurScrScale);
-
-            // Store scale when window resize is larger to smaller and no auto-detect.
-            if (_lastPixels > totalPixels && !_wasUIAutoDetect)
-                _lastScale = CurUiScale;
-
-            // Restore scale when window resize is smaller to larger and a scale was remembered.
-            if (totalPixels > _lastPixels && _lastScale > 0)
-            {
-                int scale = _lastScale;
-                _lastScale = 0;
-                returnScale = scale;
-            }
-            // Restore auto-detect if it was set and the above conditions were not met.
-            else if (_wasUIAutoDetect)
-            {
-                _wasUIAutoDetect = false;
-                _lastScale = 0;
-                returnScale = ScreenScale;
-            }
-            // Under normal circumstances calculate the new UI scale.
-            else
-            {
-                returnScale = (GameSettings.UiScale == Game1.ScreenScale)
-                    ? ScreenScale
-                    : MathHelper.Clamp(GameSettings.UiScale, 1, ScreenScale);
-            }
-            // Remember the last screen size to detect a resize event.
-            _lastPixels = totalPixels;
-            return returnScale;
-        }
-
         private void UpdateScale(bool SkipEditor = false)
         {
 
@@ -980,9 +936,15 @@ namespace ProjectZ
             {
                 GameManager.SetGameScale(GameSettings.GameScale == 11 ? gameScale : GameSettings.GameScale);
             }
-            // Calculate the UI scale with it's many nuances.
-            UiScale = GameSettings.UiScale = CalculateUiScale(CurUiScale, CurScrScale);
-
+            // Scale of the user interface.
+            if (GameSettings.UiScale > ScreenScale)
+            {
+                UiScale = ScreenScale;
+            }
+            else
+            {
+                UiScale = GameSettings.UiScale == 0 ? ScreenScale : MathHelper.Clamp(GameSettings.UiScale, 1, ScreenScale);
+            }
             // NOTE: This was used as a workaround to issues with Exclusive Fullscreen mode. Null render targets caused editor to crash on start up.
             if (SkipEditor) EditorUi.SizeChanged();
 
